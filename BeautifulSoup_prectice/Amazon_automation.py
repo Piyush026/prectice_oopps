9967af29b8341ee009fdaac74e90bf3994c21fa9
Piyush026 <57993929+Piyush026@users.noreply.github.com>
2020-02-13 11:16:58 +0530

    Delete Amazon_automation.py

--- BeautifulSoup_prectice/Amazon_automation.py
+++
@@ -1,8 +0,0 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-cred= ServiceAccountCredentials.from_json_keyfile_name('AmazonAutomation.json',scope)
-
-client = gspread.authorize(cred)
